58db838093ec57f6142cb175f90ba0e796c768e2
Doormat0608 <43817157+Doormat0608@users.noreply.github.com>
2023-05-17 09:59:27 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
+import aiogram
 from aiogram import Bot, Dispatcher, types
+from aiogram.types import Message
 from aiogram.utils import executor
-from handlers import start, attendance
+from functions import welcome, help, get_student_grades
 from config import TOKEN
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-dp.register_message_handler(start, commands=['start'])
-dp.register_message_handler(attendance, commands=['attendance'])
+dp.register_message_handler(welcome, commands=['start'])
+dp.register_message_handler(help, commands=['help'])
+dp.register_message_handler(get_student_grades)
 
 if __name__ == '__main__':
-    executor.start_polling(dp)
-
+    executor.start_polling(dp, skip_updates=True)
